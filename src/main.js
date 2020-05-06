2c375f312c2c164e585345612b91f21433739afa
xdc <1434412784@qq.com>
2020-05-06 15:02:09 +0800

    feat: 完成后台项目的通用化配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import './plugins/element.js'
 
 Vue.config.productionTip = false
 
+// axios 配置
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://192.168.31.15:8080/backstage/api/'
+//
+// 拦截器 存储token
+// axios.interceptors.request.use(config => {
+//   config.headers.Authorization = window.sessionStorage.getItem('token')
+//
+//   return config
+// })
+
+Vue.prototype.$http = axios
+
 new Vue({
   router,
   render: h => h(App)
