4ee62cfd836c4132887a3ab934dc1da06e074072
xdc <1434412784@qq.com>
2020-05-06 15:33:26 +0800

    fix: 记得要配置全局样式

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 全局样式
+import './assets/css/global.css'
+
 Vue.config.productionTip = false
 
 // axios 配置
